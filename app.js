23687792dc033d8136eadf341032a9fc94aaa4a7
jungwoonlim <wypo23@naver.com>
2019-05-01 19:46:35 +0900

    Canvas Events

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // app.js
+
+const canvas = document.getElementById(`jsCanvas`);
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function onMouseMove(event) {
+    // offset - canvas와 관련있는 값
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+function onMouseUp(event) {
+    stopPainting();
+}
+
+
+if(canvas) {
+    canvas.addEventListener(`mousemove`, onMouseMove);
+    canvas.addEventListener(`mousedown`, onMouseDown);
+    canvas.addEventListener(`mouseup`, onMouseUp);
+    canvas.addEventListener(`mouseleave`, stopPainting);
+}
+
+
+
+
+
+function init() {
+
+}
+
+init();
