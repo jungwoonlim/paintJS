e33222ae7378cac29331c13f0ac61db112b6acca
jungwoonlim <wypo23@naver.com>
2019-05-02 21:59:10 +0900

    Color Changing

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // app.js
+// canvas에 대해서 이해를 하고 싶다면 CanvasRenderingContext2D, Canvas API 참고
+
 
 const canvas = document.getElementById(`jsCanvas`);
 const ctx = canvas.getContext(`2d`);
+const colors = document.getElementsByClassName(`jsColor`);
 
 canvas.width = 500;
 canvas.height = 500;
 
-ctx.strokeStyle = "#2c2c2c";
+// ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -24,21 +27,30 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting){
+        // console.log(`creating line in ${x,y}`);
         ctx.beginPath();
         ctx.moveTo(x,y);
     } else {
+        // console.log(`creating line in ${x,y}`);
         ctx.lineTo(x,y);
         ctx.stroke();
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
-}
+// function onMouseDown(event) {
+//     painting = true;
+// }
 
 // function onMouseUp(event) {
 //     stopPainting();
 // }
+
+function handleColorClick(event) {
+    // console.log(event.target.style);
+    const color = event.target.style.backgroundColor;
+    // console.log(color);
+    ctx.strokeStyle = color;
+}
 
 
 if(canvas) {
@@ -49,11 +61,11 @@ if(canvas) {
 }
 
 
-
-
-
-function init() {
-
+// console.log(colors);
+// console.log(Array.from(colors));
+if (colors) {
+    Array.from(colors).forEach(color => 
+        color.addEventListener(`click`, handleColorClick)
+    );    
 }
 
-init();
